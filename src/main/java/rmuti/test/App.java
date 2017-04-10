cd442fefcb6e11e722f085e2e7f5261a180a7343
saharatarey2539 <saharatarey2539@gmail.com>
2017-04-10 10:57:02 +0700

    AREY-ROJECT

---
+++ src/main/java/rmuti/test/App.java
@@ -0,0 +1,9 @@
+package rmuti.test;
+
+public class App {
+	public static void main(String[] args) {
+		NumberUtil number = new NumberUtil();
+		int[] arr = { 2,3,4,5,6 };
+		System.out.println(number.sumArray(arr));
+	}
+}
